22cdf7f36ff1cc954d095bda85c97d2938de8a7c
jsungmin6 <jsungmin506@gmail.com>
2020-10-24 11:15:28 +0900

    ws 라이브러리 통해 웹 소켓 연결

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("./config/express");
 const { logger } = require("./config/winston");
 const dotenv = require('dotenv');
 dotenv.config();
+const webSoket = require('./socket')
 
-const port = 80;
-express().listen(port)
+const port = 3000;
+const server = express().listen(port)
 logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
+
+webSoket(server);
